c7cf1e5c4b5e541a374c624f8813d7e1bc0abfdd
doublenns <nnamdi.anyanwu@gmail.com>
2016-04-07 14:01:48 +0000

    Quick dirty script to check Python on remote servers

---
+++ pythonCheck.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash -
+for i in $(cat serverList) ; do
+
+        ssh     -o ConnectTimeout=10 \
+                -o UserKnownHostsFile=/dev/null \
+                -o StrictHostKeyChecking=no \
+                -o PreferredAuthentications=publickey \
+                $(whoami)@"$i" 'python --version';
+
+done
