40e07cba2c7c6b0fdb358ea3e25df0d95e639af4
Nnamdi Anyanwu <nnamdianyanwu@gmail.com>
2016-04-07 21:37:14 -0400

    Added total memory to csv_file output

--- freeSunOSMemory.py
+++ freeSunOSMemory.py
@@ -5,32 +5,42 @@ import csv
 '''
 Script grabs SunOS memory statistics and outputs as CSV
 To Do
-* Memmory Paging PiT
+* Free Memory Percentage
+* Memory Paging PiT
 * Memory Activity Page Scan PiT
 '''
 
-bashCommand = "vmstat 1 2"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-# Might want to use a "try" or call.check in case bashCommand fails
-# i.e. not a SunOS box
-output = process.communicate()[0]
-# Use the second line of cmd output as Memory Statistics
-memoryStatistics = output.split('\n')[3]
-freeMemory = memoryStatistics.split()[4]
+def run_shell_command(cmd, *shell):
+	# Might want to use a "try" or call.check in case bashCommand fails
+	# i.e. not a SunOS box
+	bash_command = cmd
+	if "shell" in shell:
+		process = subprocess.Popen(cmd,
+			stdout=subprocess.PIPE, shell=True)
+	else:
+		process = subprocess.Popen(cmd.split(),
+			stdout=subprocess.PIPE)
+	output = process.communicate()[0]
+	return output
 
-csvFile = open('memoryUtilization.csv', 'w')
-writer = csv.writer(csvFile)
 
+# Use the second line of vmstat output as Free Memory Statistics
+free_memory_statistics = run_shell_command("vmstat 1 2").split('\n')[3]
+free_memory = free_memory_statistics.split()[4]
+total_memory_statistics = run_shell_command("prtconf | grep Memory", "shell")
+total_memory = total_memory_statistics.split(":")[1].strip()
+
+csv_file = open('memoryUtilization.csv', 'w')
+writer = csv.writer(csv_file)
 data = [
-	['Total Memory', freeMemory],
-	['Free Memory', freeMemory]
+	['Name', 'Value'],
+	['Free Memory', free_memory],
+	['Total Memory', total_memory]
 ]
-
 writer.writerows(data)
-csvFile.close()
-
-exit(0)
+csv_file.close()
 
 
 
 # 'prtconf | grep Memory' cmd gives total SunOS system memory
+# 'kstat -n system_pages' cmd gives some other memory statistics
